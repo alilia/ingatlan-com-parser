444ebc2fcbb058847e8c9805ec1f8b3bccb8b57c
Ilia FILIN <ilia@filin.eu>
2018-01-13 08:21:08 +0100

    Making executable main.py.

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ import MapVisualizer as mv
 
 def main():
     """welcome to the jungle"""
-    ingatlan_com = ic.IngatlanComParser("file:///Users/ifilin/Desktop/Kiado%CC%81%20gara%CC%81zsok%20Budapest%20-%20ingatlan.com.html")
+    ingatlan_com = ic.IngatlanComParser("https://ingatlan.com/budapest/kiado+garazs")
     parsed_ingatlan_com = ingatlan_com.parse()
 
     map_visualiser = mv.MapVisualizer()
     for item in parsed_ingatlan_com:
-        map_visualiser.add(mv.MapItem(
-            address=item.address,
-            price=item.price,
-            link=item.link,
-        ))
+        pass
+    #     map_visualiser.add(mv.MapItem(
+    #         address=item["address"],
+    #         price=item["price"],
+    #         link=item["link"],
+    #     ))
 
-    map_visualiser.generate()
-    map_visualiser.display()
+    # map_visualiser.generate()
+    # map_visualiser.display()
 
     return False
 
 main()
+print "Done"
